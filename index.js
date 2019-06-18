982cae5967467dd12416efd9324e47c1d71d529b
gabagonzalez <gaba2801@gmail.com>
2019-06-18 11:40:10 -0300

    example readtext

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+
+//module.exports = () => {
+//  // ...
+//};
+//
+//
+const fs= require('fs');
+fs.readFile('archivo.text','utf-8',(error,datos)=> {
+if (error) {
+  result;
+}else{
+  console.log(datos);
+}
+});
