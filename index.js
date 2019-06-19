fbb316544b57d430fdce68bbecb91bdfc9aa9c1f
gabagonzalez <gaba2801@gmail.com>
2019-06-19 17:46:10 -0400

    pruebas reallinks

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 
-//module.exports = () => {
-//  // ...
-//};
+//primero declaración 
 
-//example
-//const fileUrl = URL('file:///tmp/hello');
-//const fileUrl = new URL('file:///tmp/hello');
-//fs.readFileSync(fileUrl);
+const marked = require("marked");
 
-//example me devuelve text del archivo
-// const fs= require('fs');
-// fs.readFile('archivo.text','utf-8',(error,datos)=> {
-// if (error) {
-//   result;
-// }else{
-//   console.log(datos);
-// }
-// });
+const fs = require('fs');
 
-//example vuelve lineas de text
-let fs = require('fs');
-let numberLines = 
-fs.readFileSync(process.argv[2],'utf8')
-.split('\n')
-.length -1;
 
-console.log(numberLines);
+
+const links = (path =>{
+  fs.readFile(path,"utf8", (err,data) =>{
+    if(err){
+      throw err
+    }
+    let links =[];
+
+    const renderer = new marked.Renderer();
+
+    renderer.link = function(href, title, text){
+
+      links.push({
+        
+        href:href,
+        // text:text,
+        // file:path,
+        // title:title
+      
+      })
+
+    }
+    marked(data, {renderer:renderer})
+      console.log(links)
+  })
+
+})
+
+console.log(links("./prueba.md"));
