53083d75761983f5a8247100f35ebe0677ea4829
gabagonzalez <gaba2801@gmail.com>
2019-06-19 12:28:33 -0300

    dependencias y example2

--- index.js
+++ index.js
@@ -2,20 +2,27 @@
 //module.exports = () => {
 //  // ...
 //};
-//
-//
+
+//example
 //const fileUrl = URL('file:///tmp/hello');
 //const fileUrl = new URL('file:///tmp/hello');
 //fs.readFileSync(fileUrl);
 
-const fs= require('fs');
+//example me devuelve text del archivo
+// const fs= require('fs');
+// fs.readFile('archivo.text','utf-8',(error,datos)=> {
+// if (error) {
+//   result;
+// }else{
+//   console.log(datos);
+// }
+// });
 
-fs.readFile('archivo.text','utf-8',(error,datos)=> {
-if (error) {
-  result;
-}else{
-  console.log(datos);
-}
-});
+//example vuelve lineas de text
+let fs = require('fs');
+let numberLines = 
+fs.readFileSync(process.argv[2],'utf8')
+.split('\n')
+.length -1;
 
-//
+console.log(numberLines);
